eeb79067d6813f78e6342433a26857158322ef3f
ta4ilka69 <artem_balin_2004@mail.ru>
2021-04-04 23:18:58 +0300

    Доработан скрипт звонков

--- client/test.py
+++ client/test.py
@@ -3,48 +3,46 @@ import schedule
 import requests
 import time
 import playsound
-import threading
-global id, enable, lessons
+global id, lessons
 lessons = []
 id = 0
 
 def response():
-    q = 1
-    dayOfWeek = int(time.strftime("%w"))
-    global id, lessons, enable
-    if 0<dayOfWeek<6:
-        res = requests.get(f"http://localhost:3000/test?id={dayOfWeek}") # получаем объект с ссылки
+    global id, lessons
+    id = int(time.strftime("%w"))
+    if id == 0:
+        id = 7
+    if 0<id<8:
+        res = requests.get(f"http://localhost:3000/day?dayOfWeek={id}") # получаем объект с ссылки
         obj = json.loads(res.text) # преобразуем в объект (массив) python
-        id = dayOfWeek
-        enable = bool(obj["enabled"])
-        lessons = []
-        for t in obj["lessons"]:
-            q = list(t.split())
-            lessons.append(q)
+        lessons.clear()
+        for t in obj:
+            start = str(t["timeStart"])
+            end = str(t["timeEnd"])
+            lessons.append([start,end])
+        print(lessons)
     else:
         id = 0
 
 
 def check():
-    q = 1
-    if 0<id<6:
-        if enable:
-            m = str(time.strftime("%M"))
-            h = str(time.strftime("%H"))
-            for t in lessons:
-                if t[0] == h+':'+m:
-                    playsound.playsound('./static/sounds/sfx1.mp3')
-                    print('bell')
-                    time.sleep(15)
-                    playsound.playsound('./static/sounds/sfx1.mp3')
-                    time.sleep(45)
+    if 0<id<8:
+        m = str(time.strftime("%M"))
+        h = str(time.strftime("%H"))
+        if m == '9':
+            response()
+            print('response', time.time("%H%M"))
+        for t in lessons:
+            if t[0] == h+':'+m:
+                playsound.playsound('./client/sounds/sfx1.mp3')
+                time.sleep(15)
+                playsound.playsound('./client/sounds/sfx1.mp3')
+                time.sleep(45)
+            elif t[1] == h+':'+m:
+                playsound.playsound('./client/sounds/sfx2.mp3')
+                time.sleep(60)
 
-                elif t[1] == h+':'+m:
-                    playsound.playsound('./static/sounds/sfx2.mp3')
-                    print('bell')
-                    time.sleep(60)
-
-schedule.every(1).minutes.do(response)
+response()
 schedule.every(1).seconds.do(check)
 
 while True:
