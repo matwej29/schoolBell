226193a2db6653ad4aff245d8154fc6d18391115
ta4ilka69 <artem_balin_2004@mail.ru>
2021-04-12 22:50:54 +0300

    add config.json and edit python file

--- client/test.py
+++ client/test.py
@@ -3,44 +3,52 @@ import schedule
 import requests
 import time
 import playsound
+
 global id, lessons
 
-lessons = [] # список со звонками за 1 день
-id = 0 # номер дня (варьируется с 0 до 7)
+lessons = []  # список со звонками за 1 день
+id = 0  # номер дня (варьируется с 0 до 7)
+
 
 def response():
     global id, lessons
-    id = int(time.strftime("%w")) # переприсваиваем id
-    if id == 0:# если воскресенье, то у python - 0
-        id = 7 # а у базы данных - 7
-    if 0<id<8: # проверка дня недели, нет смысла выполнять скрипт по воскресеньям
-        res = requests.get(f"http://localhost:3000/day?dayOfWeek={id}") # получаем объект с ссылки
-        obj = json.loads(res.text) # преобразуем в объект (массив) python
-        lessons.clear() # очищаем массив
-        for t in obj: #добавляем в массив данные об уроке в виде [начало, конец]
+    id = int(time.strftime("%w"))  # переприсваиваем id
+    if id == 0:  # если воскресенье, то у python - 0
+        id = 7  # а у базы данных - 7
+    if 0 < id < 8:  # проверка дня недели, нет смысла выполнять скрипт по воскресеньям
+        conf = open("./client/config.json", "r").read()  # получаем конфиг
+        connection_config = json.loads(conf)  # преобразуем в объект python
+        ip = connection_config["ip"]
+        port = connection_config["port"]
+        res = requests.get(f"http://{ip}:{port}/day?dayOfWeek={id}")  # получаем объект с ссылки
+        obj = json.loads(res.text)  # преобразуем в объект (массив) python
+        lessons.clear()  # очищаем массив
+        for t in obj:  # добавляем в массив данные об уроке в виде [начало, конец]
             start = str(t["timeStart"])
             end = str(t["timeEnd"])
-            lessons.append([start,end])
+            lessons.append([start, end])
+
 
 # функция сверяет время с началом или концом урока и дает соответствующий звонок (звонки)
 def check():
-    if 0<id<8: # проверка дня недели, нет смысла выполнять скрипт по воскресеньям
-        m = str(time.strftime("%M")) # получаем минуты [0-59]
-        h = str(time.strftime("%H")) # получем часы [0-23]
-        if m[-1] == '9': # если минуты оканчиваются на 9, 
-            response()   # то делается обновление расписания (в итоге - раз в 10 минут)
+    if 0 < id < 8:  # проверка дня недели, нет смысла выполнять скрипт по воскресеньям
+        m = str(time.strftime("%M"))  # получаем минуты [0-59]
+        h = str(time.strftime("%H"))  # получем часы [0-23]
+        if m[-1] == "9":  # если минуты оканчиваются на 9,
+            response()  # то делается обновление расписания (в итоге - раз в 10 минут)
         for t in lessons:
-            if t[0] == h+':'+m: # проверка на начало урока
-                playsound.playsound('./client/sounds/sfx1.mp3') # первый звонок
-                time.sleep(15) # ожидаем 15 секунд
-                playsound.playsound('./client/sounds/sfx1.mp3') # второй звонок
-                time.sleep(45) # ожидаем еще 45 секунд, иначе звонки повторяться (нужно, чтобы время перешло на след. минуту)
-            elif t[1] == h+':'+m:   # проверка на перемену
-                playsound.playsound('./client/sounds/sfx2.mp3') # звонок и ожидание 60 секунд
+            if t[0] == h + ":" + m:  # проверка на начало урока
+                playsound.playsound("./client/sounds/sfx1.mp3")  # первый звонок
+                time.sleep(15)  # ожидаем 15 секунд
+                playsound.playsound("./client/sounds/sfx1.mp3")  # второй звонок
+                time.sleep(45)  # ожидаем еще 45 секунд, иначе звонки повторяться (нужно, чтобы время перешло на след. минуту)
+            elif t[1] == h + ":" + m:  # проверка на перемену
+                playsound.playsound("./client/sounds/sfx2.mp3")  # звонок и ожидание 60 секунд
                 time.sleep(60)
 
-response() # получение расписания сразу после запуска
-schedule.every(1).seconds.do(check) # делаем функцию check каждую секунду
+
+response()  # получение расписания сразу после запуска
+schedule.every(1).seconds.do(check)  # делаем функцию check каждую секунду
 
 while True:
-    schedule.run_pending() # непрерываемая функция
+    schedule.run_pending()  # непрерываемая функция
